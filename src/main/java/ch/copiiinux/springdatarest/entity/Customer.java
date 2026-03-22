7ca81cc02dd2e90a81120c4d429e58b91dfc1fef
copinux <loick2610@gmail.com>
2026-03-22 20:08:47 +0100

    Add validation annotations and all-args constructor to Customer entity
    Signed-off-by: Loïck Roth <loick2610@gmail.com>

--- src/main/java/ch/copiiinux/springdatarest/entity/Customer.java
+++ src/main/java/ch/copiiinux/springdatarest/entity/Customer.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,10 +15,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
     @Id
+    @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotNull
+    @Size(min = 2, max = 64)
     private String name;
 }
 
