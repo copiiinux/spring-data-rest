5aa98f78f55c303fc2c9419fa6849b6d7681e401
Loïck Roth <loick2610@gmail.com>
2026-03-22 22:51:21 +0100

    Replace ListCrudRepository with JpaRepository in CustomerRepository interface
    Signed-off-by: Loïck Roth <loick2610@gmail.com>

--- src/main/java/ch/copiiinux/springdatarest/repository/CustomerRepository.java
+++ src/main/java/ch/copiiinux/springdatarest/repository/CustomerRepository.java
@@ -1,7 +1,7 @@
 package ch.copiiinux.springdatarest.repository;
 
 import ch.copiiinux.springdatarest.entity.Customer;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends ListCrudRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
